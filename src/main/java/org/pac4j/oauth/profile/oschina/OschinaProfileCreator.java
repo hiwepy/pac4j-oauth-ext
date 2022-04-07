e1da613508923581a13efddf35340773eac380cc
wandl <hiwepy@163.com>
2022-04-07 10:13:14 +0800

    pac4j.version>4.5.5

--- src/main/java/org/pac4j/oauth/profile/oschina/OschinaProfileCreator.java
+++ src/main/java/org/pac4j/oauth/profile/oschina/OschinaProfileCreator.java
@@ -1,5 +1,7 @@
 package org.pac4j.oauth.profile.oschina;
 
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.oauth.OAuth20Service;
 import org.pac4j.core.client.IndirectClient;
 import org.pac4j.oauth.config.OAuth20Configuration;
 import org.pac4j.oauth.profile.creator.OAuth20ProfileCreator;
@@ -13,10 +15,10 @@ public class OschinaProfileCreator extends OAuth20ProfileCreator {
 	public OschinaProfileCreator(OAuth20Configuration configuration, IndirectClient client) {
 		super(configuration, client);
 	}
-	
+
 	@Override
-	protected void signRequest(final OAuthService service, final Token token, final OAuthRequest request) {
-		super.signRequest(service, token, request);
+	protected void signRequest(final OAuth20Service service, final OAuth2AccessToken accessToken, final OAuthRequest request) {
+		super.signRequest(service, accessToken, request);
 		// 指定返回值类型['json'|'jsonp'|'xml']
 		request.addQuerystringParameter("dataType", "json");
 	}
