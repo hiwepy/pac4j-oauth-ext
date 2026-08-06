65441ec00938208f12c50c0dec1cf64040a92be1
wandl-6A72h <hiwepy@gmail.com>
2026-08-06 14:59:50 +0800

    chore: 注释作者信息统一为 @Loong Wan

--- src/main/java/org/pac4j/oauth/profile/yiban/YibanProfile.java
+++ src/main/java/org/pac4j/oauth/profile/yiban/YibanProfile.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, Loong Wan (https://github.com/loong10k).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -27,7 +27,7 @@ import org.pac4j.oauth.profile.weibo.WeiboProfileDefinition;
  * <p>This class is the user profile for Yiban (using OAuth protocol version 2) with appropriate getters.</p>
  * <p>It is returned by the {@link org.pac4j.oauth.client.YibanClient}.</p>
  *
- * @author 		： <a href="https://github.com/hiwepy">wandl</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class YibanProfile extends OAuth20Profile {
 
--- src/main/java/org/pac4j/scribe/service/YibanService.java
+++ src/main/java/org/pac4j/scribe/service/YibanService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, Loong Wan (https://github.com/loong10k).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -34,7 +34,7 @@ import com.github.scribejava.core.oauth.OAuth20Service;
  * As of scribejava 5.3, the enumeration class ClientAuthenticationType does not support inheritance,
  * and can not complete the client authentication of Yiban.
  *
- * @author 		： <a href="https://github.com/hiwepy">wandl</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class YibanService extends OAuth20Service {
     
