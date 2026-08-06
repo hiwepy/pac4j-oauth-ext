592e94ce1fac3c8360b540f4e05678d2bc990421
wandl-6A72h <hiwepy@gmail.com>
2026-08-06 14:59:51 +0800

    chore: 注释作者信息统一为 @Loong Wan

--- src/main/java/org/pac4j/oauth/profile/oschina/OschinaProfile.java
+++ src/main/java/org/pac4j/oauth/profile/oschina/OschinaProfile.java
@@ -9,7 +9,7 @@ import org.pac4j.oauth.profile.OAuth20Profile;
  * <p>This class is the user profile for Oschina (using OAuth protocol version 2) with appropriate getters.</p>
  * <p>It is returned by the {@link org.pac4j.oauth.client.OschinaClient}.</p>
  *
- * @author 		： <a href="https://github.com/hiwepy">wandl</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class OschinaProfile extends OAuth20Profile {
 
--- src/main/java/org/pac4j/scribe/model/YibanToken.java
+++ src/main/java/org/pac4j/scribe/model/YibanToken.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, hiwepy (https://github.com/hiwepy).
+ * Copyright (c) 2018, Loong Wan (https://github.com/loong10k).
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -23,7 +23,7 @@ import org.pac4j.util.MyCommonHelper;
 /**
  * Yiban token extra.
  * <p>More info at: <a href="https://open.yiban.cn/wiki/index.php?page=oauth/access_token"> access_token</a></p>
- * @author 		： <a href="https://github.com/hiwepy">wandl</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class YibanToken extends OAuth2AccessToken {
 
